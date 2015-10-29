61aa95a6c6e94d40e33b61a365aad427d620336b
Julian Rosser <julianrosser@gmail.com>
2015-10-29 15:12:17 +0000

    added readme

--- R/calcPosteriorMap.R
+++ R/calcPosteriorMap.R
@@ -1,11 +1,20 @@
 #' Calc Posterior Map Function
 #'
-#' This function calculates the posterior map based on weights of evidence rasters
-#' @param evidenceRasters are multiple classifier rasters according to weighted evidence
+#' This function calculates the posterior map based on weights of evidence rasters. The function returns a raster object.
+#' @param evidenceRasters (RasterBrick) are multiple classifier rasters according to weighted evidence
 #' @keywords WoE, Weights of Evidence
-#' @export
+#' @export 
 #' @examples
 #' calcWeights()
 calcPosteriorMap = function (evidenceRasters) {
 
+	#compute prior logit value
+	
+	#create raster of prilogit value, priLogitRast equal to evidenceRasters dimension
+	
+	#posteriorLogitRaster = map calc sum of priLogitRast and evidenceRasters
+
+	#posterOdds = exp(posteriorLogitRaster)
+	
+	#posteriorProb = posterOdds/(1+posterOdds)
 }
---
+++ readme.md
@@ -0,0 +1,4 @@
+# woer R package #
+### Some R functions for computing weights of evidence##
+
+Weights of evidence caluclation for spatial data following Bonham-Carter, 1994.
